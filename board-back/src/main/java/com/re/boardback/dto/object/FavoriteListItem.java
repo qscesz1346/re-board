68d640ad0a836dbe9791f10e1bb946bfda4d241e
qscesz1346 <qscesz1346@naver.com>
2024-01-20 00:03:08 +0900

    2024.01.20

---
+++ board-back/src/main/java/com/re/boardback/dto/object/FavoriteListItem.java
@@ -0,0 +1,16 @@
+package com.re.boardback.dto.object;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FavoriteListItem {
+    
+    private String email;
+    private String nickname;
+    private String profileImage;
+
+}
